206bf217ed6ad0c456bacd92deb6b9646e62dd00
Vasishth Gupta <vasishthemail@gmail.com>
2023-05-19 18:17:29 +0530

    cray

--- static/main.js
+++ static/main.js
@@ -26,3 +26,31 @@ $('#signupform').submit(function(event) {
       }
     });
 });
+
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+function onMouseMove(e) {
+    $(".cursor").css("display", "block")
+  TweenMax.to($smallBall, .4, {
+    x: e.clientX - 10,
+    y: e.clientY - 10
+  })
+}
+
+function onMouseHover() {
+  TweenMax.to($smallBall, .3, {
+    scale: 2
+  })
+}
+function onMouseHoverOut() {
+  TweenMax.to($smallBall, .3, {
+    scale: 1
+  })
+}
