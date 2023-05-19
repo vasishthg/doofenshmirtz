145f00f6972b80cce2d4540ce0c3b895e6d58703
VasishthG <76578694+vasishthg@users.noreply.github.com>
2023-05-19 12:00:50 +0530

    ee

--- static/main.js
+++ static/main.js
@@ -9,3 +9,20 @@ $('.nav-menubtnimg').click(function(){
 $(".menu-backbtn").click(function(){
     $(".menu").hide("slide", {direction: "left"}, 700)
 })
+if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+  }
+$('#signupform').submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/post/signup',
+      data: $(this).serialize(),
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+});
--- xino.py
+++ xino.py
@@ -14,10 +14,22 @@ app.config['MYSQL_PASSWORD'] = 'toor'
 
 mysql = MySQL(app)
 
+@app.route("/post/signup", methods=["POST", "GET"])
+def ajaxsignup():
+    name = request.form.get("signup-name")
+    email = request.form.get("signup-email")
+    username = request.form.get("signup-username")
+    password = request.form.get("signup-password")
+    dd = int(request.form.get("dob-dd"))
+    mm = int(request.form.get("dob-mm"))
+    yyyy = int(request.form.get("dob-yyyy"))
+    return 0
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     return render_template('index.html')
 
-
+if __name__ == "__main__":    
+    app.run(debug=True)
 app.run(debug=True)
