7547bb55fe95993940f70f8106a9224dc3e0e636
Vasishth Gupta <vasishthemail@gmail.com>
2023-05-21 22:00:11 +0530

    e

--- static/main.js
+++ static/main.js
@@ -1,31 +1,10 @@
-(function(){
-    var scroll = new LocomotiveScroll();
-})();
+// (function(){
+//     const scroll = new LocomotiveScroll({
+//       el: document.querySelector('[data-scroll-container]'),
+//       smooth: true,
+//     })
+// })();
 
-$('.nav-menubtnimg').click(function(){
-    $('.menu').show("slide", {direction: "right"}, 700)
-})
-
-$(".menu-backbtn").click(function(){
-    $(".menu").hide("slide", {direction: "left"}, 700)
-})
-if ( window.history.replaceState ) {
-    window.history.replaceState( null, null, window.location.href );
-  }
-$('#signupform').submit(function(event) {
-    event.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: '/post/signup',
-      data: $(this).serialize(),
-      success: function(response) {
-        console.log(response);
-      },
-      error: function(error) {
-        console.log(error);
-      }
-    });
-});
 
 const $smallBall = document.querySelector('.cursor__ball--small');
 const $hoverables = document.querySelectorAll('.hoverable');
@@ -54,3 +33,387 @@ function onMouseHoverOut() {
     scale: 1
   })
 }
+
+$('.nav-menubtnimg').click(function(){
+    $('.menu').show("slide", {direction: "right"}, 700)
+})
+
+$(".menu-backbtn").click(function(){
+    $(".menu").hide("slide", {direction: "left"}, 700)
+})
+if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+  }
+$('#signupform').submit(function(event) {
+    event.preventDefault();
+    var dd = parseInt(document.getElementById("dob-dd").value);
+  var mm = parseInt(document.getElementById("dob-mm").value);
+  var yyyy = parseInt(document.getElementById("dob-yyyy").value);
+  
+  if (isNaN(dd) || isNaN(mm) || isNaN(yyyy) || dd < 1 || dd > 31 || mm < 1 || mm > 12) {
+    alert("Please enter a valid date.");
+    return;
+  }
+  
+  if (mm == 2) { // February
+    if (dd > 29) {
+      alert("Please enter a valid date for February.");
+      return;
+    }
+    
+    if (dd == 29 && (yyyy % 4 !== 0 || (yyyy % 100 === 0 && yyyy % 400 !== 0))) {
+      alert("Please enter a valid date for February in a non-leap year.");
+      return;
+    }
+  }
+  else{
+    $.ajax({
+      type: 'POST',
+      url: '/post/signup',
+      data: $(this).serialize(),
+      
+      success: function(response) {
+        $("#smsg").text("Signed up successfully!")
+      $(".notif-s").fadeIn(600)
+      document.querySelector(".notif-s").setAttribute("class", "notif-success animate__fadeInLeft")
+      console.log(response);
+      setTimeout(function(){
+        window.location.href = window.location
+      }, 2000)
+      },
+      error: function(error) {
+        console.log(error['status'])
+      // error = "("+error['status'] + ") " + error['statusText']
+      $(".notif-e").fadeIn(600)
+      document.querySelector(".notif-e").setAttribute("class", "notif-e notif error animate__fadeInLeft")
+      $(".notif-e").css("z-index", "999")
+      document.getElementById("emsg").innerHTML = error['statusText'];
+      document.getElementById("ecode").innerHTML = error['status'];
+      setTimeout(function(){
+        $(".notif-e").fadeOut(600)
+
+        document.querySelector(".notif-e").setAttribute("class", "notif-e notif error animate__fadeOutLeft")
+      }, 3000)
+      }
+    });
+    // success: function(response) {
+      $("#smsg").text("You have been logged in!")
+      $(".notif-s").fadeIn(600)
+      document.querySelector(".notif-s").setAttribute("class", "notif-success animate__fadeInLeft")
+      console.log(response);
+      setTimeout(function(){
+        window.location.href = window.location
+      }, 2000)
+  }
+});
+
+$('.loggedinbtn, .team-followbtn').click(function(event) {
+  const oid = this.id
+  id = ((this.id).replace('team-', ''))
+  if ($(".team-followbtn").hasClass("following")){
+    return
+  }
+  else{
+
+    $.ajax({
+      type: 'POST',
+      url: '/post/team/'+id,
+      data: id,
+      success: function(response) {
+        $('#' + oid).text("Following")
+        $("#" + oid).addClass("following")
+        $("#" + oid).animate({
+          backgroundColor: "white",
+          color: "black"
+        }, 700)
+        console.log(response);
+        
+      },
+      error: function(error) { 
+        console.log(error);
+      }
+    });
+  }
+});
+
+
+
+
+
+$('#loginform').submit(function(event) {
+  event.preventDefault();
+  $.ajax({
+    type: 'POST',
+    url: '/post/login',
+    data: $(this).serialize(),
+    success: function(response) {
+      $("#smsg").text("You have been logged in!")
+      $(".notif-s").fadeIn(600)
+      document.querySelector(".notif-s").setAttribute("class", "notif-success animate__fadeInLeft")
+      console.log(response);
+      setTimeout(function(){
+        window.location.href = window.location
+      }, 2000)
+    },
+    error: function(error) {
+      console.log(error['status'])
+      if (error['statusText'] === "CONFLICT"){
+        error['statusText'] = "Invalid Credentials"
+      }
+      // error = "("+error['status'] + ") " + error['statusText']
+      $(".notif-e").fadeIn(600)
+      document.querySelector(".notif-e").setAttribute("class", "notif-e notif error animate__fadeInLeft")
+      $(".notif-e").css("z-index", "999")
+      document.getElementById("emsg").innerHTML = error['statusText'];
+      document.getElementById("ecode").innerHTML = error['status'];
+      setTimeout(function(){
+        $(".notif-e").fadeOut(600)
+
+        document.querySelector(".notif-e").setAttribute("class", "notif-e notif error animate__fadeOutLeft")
+      }, 3000)
+    }
+  });
+});
+
+$(".nav-profileimg").click(function(){
+  $(this).css("postion", "absolute")
+  $(".profile-menu").css("zIndex", "999"); 
+  setTimeout(function(){
+    $(".profile-menu").show("slide", {direction: "left"}, 700)
+  }, 500)
+  $(".nav-profileimg").animate({
+    width: "22em",
+    height: "22em",
+    borderRadius: "50%",
+    left: "1.5em",
+    top: "2em",
+    zIndex: "1000",
+  }, 1200),
+  $("#nav-profile").css("z-index", "1999")
+  $(".nav-profileimg").css("z-index", "1999")
+    $("#nav-profile").css("position", "fixed")
+    $(".nav-profileimg").css("position", "fixed")
+    $(".nav-profileimg").css("margin-left", "2em")
+    $(".nav-profileimg").css("margin-top", "2em")
+    $(".nav-logoimg").css("z-index", "1000")
+  
+  $(".nav-logoimg").animate({
+    zIndex: "1000",
+    left: "2.5em",
+  }, 1200)
+  $(".nav-menubtnimg").animate({
+    opacity: "0",
+  }, 60)
+})
+
+
+
+$(".profile-backbtn").click(function(){
+  $(this).css("postion", "absolute")
+  $(".profile-menu").css("zIndex", "999"); 
+  setTimeout(function(){
+    $(".profile-menu").hide("slide", {direction: "left"}, 700)
+  }, 500)
+  $(".nav-profileimg").animate({
+    width: "2.5em",
+    height: "2.5em",
+    borderRadius: "50%",
+    left: "-2.5em",
+    top: "-2.5em",
+    zIndex: "0",
+  }, 1200),
+  setTimeout(function(){
+    $("#nav-profile").css("z-index", "0")
+    $(".nav-profileimg").css("z-index", "0")
+  }, 1200)
+    $("#nav-profile").css("position", "relative")
+    $(".nav-profileimg").css("position", "relative")
+    $(".nav-profileimg").css("margin-left", "2em")
+    $(".nav-profileimg").css("margin-top", "2em")
+  
+  $(".nav-logoimg").animate({
+    zIndex: "0",
+    left: "50%",
+  }, 1200)
+  $(".nav-menubtnimg").animate({
+    opacity: "100",
+  }, 60)
+})
+function closeProfile(){
+  $(this).css("postion", "absolute")
+  $(".profile-menu").css("zIndex", "999"); 
+  setTimeout(function(){
+    $(".profile-menu").hide("slide", {direction: "left"}, 700)
+  }, 500)
+  $(".nav-profileimg").animate({
+    width: "2.5em",
+    height: "2.5em",
+    borderRadius: "50%",
+    left: "-2.5em",
+    top: "-2.5em",
+    zIndex: "0",
+  }, 1200),
+  setTimeout(function(){
+    $("#nav-profile").css("z-index", "0")
+    $(".nav-profileimg").css("z-index", "0")
+  }, 1200)
+    $("#nav-profile").css("position", "relative")
+    $(".nav-profileimg").css("position", "relative")
+    $(".nav-profileimg").css("margin-left", "2em")
+    $(".nav-profileimg").css("margin-top", "2em")
+  
+  $(".nav-logoimg").animate({
+    zIndex: "0",
+    left: "50%",
+  }, 1200)
+  $(".nav-menubtnimg").animate({
+    opacity: "100",
+  }, 60)
+}
+
+$(".to-login").click(function(){
+  $(".login").show("slide", {direction: "left"}, 700)
+  $(".signup").hide("slide", {direction: "right"}, 700)
+
+  closeProfile()
+})
+
+$(".to-signup").click(function(){
+  $(".signup").show("slide", {direction: "left"}, 700)
+  $(".login").hide("slide", {direction: "right"}, 700)
+
+  closeProfile()
+})
+
+$('.not-loggedin-follow').click(function(){
+  $(".notif-s").css("z-index", "99")
+  $(".login").show("slide", {direction: "right"}, 700)
+  $("#smsg").text("Login to follow")
+  $("#ns-s").text("ALERT")
+  $("#scode").text("")
+  $(".notif-s").show("slide", {direction: "right"}, 700)
+  setTimeout(function(){
+    $(".notif-s").hide("slide", {direction: "right"}, 700)
+  }, 2000)
+})
+
+$(".following").hover(function(){
+  $(this).text("Unfollow")
+})
+
+$(".following").click(function(){
+  
+  $.ajax({
+    type: 'POST',
+    url: '/post/event/unfollow',
+    data: this.id,
+    
+    success: function(response) {
+      $(this).text("Follow")
+  $(this).animate({
+    backgroundColor: "transparent",
+    stroke: "white",
+    color: "white",
+  }, 500)
+  setTimeout(function(){
+    document.querySelector(".following").setAttribute("class", "team-followbtn loggedinbtn hoverable")
+  }, 500)
+      $("#smsg").text("You have unfollowed this team")
+    $(".notif-s").fadeIn(600)
+    document.querySelector(".notif-s").setAttribute("class", "notif-success animate__fadeInLeft")
+    console.log(response);
+    setTimeout(function(){
+      window.location.href = window.location
+    }, 2000)
+    },
+    error: function(error) {
+      console.log(error['status'])
+    // error = "("+error['status'] + ") " + error['statusText']
+    $(".notif-e").fadeIn(600)
+    document.querySelector(".notif-e").setAttribute("class", "notif-e notif error animate__fadeInLeft")
+    $(".notif-e").css("z-index", "999")
+    document.getElementById("emsg").innerHTML = error['statusText'];
+    document.getElementById("ecode").innerHTML = error['status'];
+    setTimeout(function(){
+      $(".notif-e").fadeOut(600)
+
+      document.querySelector(".notif-e").setAttribute("class", "notif-e notif error animate__fadeOutLeft")
+    }, 3000)
+    }
+  });
+})
+
+$(".following").mouseleave(function(){
+  $(this).text("Following")
+})
+
+$("#logout").click(function(){
+  window.location.href='/logout'
+})
+const carouselItems = document.querySelectorAll('.carousel-item');
+const carouselContainer = document.querySelector('.carousel-container');
+let isScrolling = false;
+let startX;
+let scrollLeft;
+
+// Set the first item as the default focus
+carouselItems[0].focus();
+
+// Center the focused item when clicked or swiped
+carouselContainer.addEventListener('mousedown', handleSwipeStart);
+carouselContainer.addEventListener('mousemove', handleSwipeMove);
+carouselContainer.addEventListener('mouseup', handleSwipeEnd);
+carouselContainer.addEventListener('mouseleave', handleSwipeEnd);
+carouselContainer.addEventListener('touchstart', handleSwipeStart);
+carouselContainer.addEventListener('touchmove', handleSwipeMove);
+carouselContainer.addEventListener('touchend', handleSwipeEnd);
+
+function handleSwipeStart(event) {
+  isScrolling = true;
+  startX = event.pageX || event.touches[0].pageX;
+  scrollLeft = carouselContainer.scrollLeft;
+}
+
+function handleSwipeMove(event) {
+  if (!isScrolling) return;
+  event.preventDefault();
+  const x = event.pageX || event.touches[0].pageX;
+  const walk = (x - startX) * 1.5; // Adjust the swipe sensitivity here
+  carouselContainer.scrollLeft = scrollLeft - walk;
+}
+
+function handleSwipeEnd() {
+  isScrolling = false;
+  const containerWidth = carouselContainer.offsetWidth;
+  const currentScrollPos = carouselContainer.scrollLeft;
+  const containerCenter = containerWidth / 2;
+  let closestItem = null;
+  let closestDistance = Infinity;
+
+  carouselItems.forEach(item => {
+    const itemRect = item.getBoundingClientRect();
+    const itemLeft = itemRect.left - carouselContainer.getBoundingClientRect().left;
+    const itemCenter = itemLeft + itemRect.width / 2;
+    const distance = Math.abs(itemCenter - containerCenter);
+
+    if (distance < closestDistance) {
+      closestDistance = distance;
+      closestItem = item;
+    }
+  });
+
+  if (closestItem) {
+    const itemRect = closestItem.getBoundingClientRect();
+    const itemLeft = itemRect.left - carouselContainer.getBoundingClientRect().left;
+    const itemCenter = itemLeft + itemRect.width / 2;
+    const scrollOffset = itemCenter - containerCenter;
+    carouselContainer.scrollTo({
+      left: currentScrollPos + scrollOffset,
+      behavior: 'smooth'
+    });
+  }
+}
+
+$("#date-tue").click(function(){
+  
+})
--- xino.py
+++ xino.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session,send_file
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from zenora import APIClient
+import os
 from werkzeug.utils import secure_filename
 import mysql.connector
+import requests
 import mysql
+import json
+import subprocess
+from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "xino"
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DB'] = 'xino'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'toor'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['BASIC_AUTH_USERNAME'] = 'upload'
+app.config['BASIC_AUTH_PASSWORD'] = 'xino-videoupload'
+basic_auth = BasicAuth(app)
 
+headers = {
+            "X-RapidAPI-Key": "1700480626msh4d4f83cf6b26be5p1c6c1fjsn5804ffc22235",
+            "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
+        }
 mysql = MySQL(app)
 
 @app.route("/post/signup", methods=["POST", "GET"])
@@ -23,11 +37,279 @@ def ajaxsignup():
     dd = int(request.form.get("dob-dd"))
     mm = int(request.form.get("dob-mm"))
     yyyy = int(request.form.get("dob-yyyy"))
-    return 0
+    dob = str(yyyy) + "-" + str(mm) + "-" + str(dd)
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM users WHERE username=%s", (username,))
+    if cur.fetchone():
+        return str(), 409
+    cur.execute("INSERT INTO users (name, email, username, password, dob) VALUES (%s, %s, %s, %s, %s)", (name, email, username, password, dob)) 
+    mysql.connection.commit()
+    cur.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    user = cur.fetchone()
+    if user:
+        session['user'] = user
+        session['id'] = user['id']
+        session['loggedin'] = True
+        session['email'] = user['email']
+        return "ee", 200
+    return "ee"
+
+@app.route("/post/login", methods=["POST", "GET"])
+def ajaxlogin():
+    username = request.form.get("login-email")
+    password = request.form.get("login-password")
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    user = cur.fetchone()
+    if user:
+        session['user'] = user
+        session['id'] = user['id']
+        session['loggedin'] = True
+        session['email'] = user['email']
+        return "ee", 200
+    else:
+        return "error", 409
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    session.pop('user', None)
+    session.pop('id', None)
+    session.pop('loggedin', None)
+    session.pop('email', None)
+    return redirect('/')
+
+@app.route('/post/team/<int:id>', methods=['GET', 'POST'])
+def team(id):
+    if 'loggedin' in session:
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        # remove prev team follow
+        cur.execute("SELECT following FROM users WHERE email = %s", [session['email']])
+        follteam = cur.fetchone()
+        if follteam['following'] != None:
+            cur.execute("SELECT * FROM teams WHERE teamabbrv = %s", [follteam['following']])
+            targetteam=cur.fetchone()
+            cur.execute("UPDATE teams SET followers = %s WHERE teamabbrv = %s", [targetteam['followers']-1 ,targetteam['teamabbrv']])
+            mysql.connection.commit()
+        # set new team shit
+        cur.execute("SELECT * FROM teams WHERE id = %s", [id])
+        team = cur.fetchone()
+        cur.execute("UPDATE users SET following=%s WHERE email = %s", (team['teamabbrv'], session['email']))
+        mysql.connection.commit()
+        cur.execute("UPDATE teams SET followers=%s WHERE teamabbrv = %s", (team['followers']+1, team['teamabbrv']))
+        mysql.connection.commit()
+
+        return 'ee', 200
+    return "error", 510
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if 'loggedin' in session:
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM users WHERE email=%s", (session['email'],))
+        user = cur.fetchone()
+        cur.execute("SELECT * FROM teams WHERE id=%s", (user['following'],))
+        team = cur.fetchone()
+        return render_template('index.html', user=user, team=team)
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     return render_template('index.html')
 
+@app.route('/updates', methods=['GET', 'POST'])
+def updates():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if 'loggedin' in session:
+        cur.execute("SELECT * FROM teams")
+        teams = cur.fetchall()
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM users WHERE email=%s", (session['email'],))
+        user = cur.fetchone()
+        cur.execute("SELECT * FROM teams WHERE id=%s", (user['following'],))
+        team = cur.fetchone()
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        livematches = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live"
+
+        
+
+        lmresponse = requests.get(livematches, headers=headers)
+        lmresponse = lmresponse.json()
+        series = "https://cricbuzz-cricket.p.rapidapi.com/series/v1/5945"
+        srsresponse = requests.get(series, headers=headers)
+        srsresponse = srsresponse.json()
+        
+        ipl_matches = []
+        
+        for match_type in lmresponse['typeMatches']:
+            if match_type['matchType'] == 'League':
+                for series_match in match_type['seriesMatches']:
+                    if series_match['seriesAdWrapper']['seriesName'] == 'Indian Premier League 2023':
+                        for match in series_match['seriesAdWrapper']['matches']:
+                            ipl_matches.append(match)
+        for match in ipl_matches:
+            series_name = match["matchInfo"]["seriesName"]
+            print(series_name)
+            match_id = match["matchInfo"]["matchId"]
+            match_format = match["matchInfo"]["matchFormat"]
+            state = match["matchInfo"]["state"]
+            status = match["matchInfo"]["status"]
+            team1_name = match["matchInfo"]["team1"]["teamName"]
+            team2_name = match["matchInfo"]["team2"]["teamName"]
+            team1_sname = match["matchInfo"]["team1"]["teamSName"]
+            team2_sname = match["matchInfo"]["team2"]["teamSName"]
+            if 'matchScore' in match:
+                match_score = match['matchScore']["team1Score"]["inngs1"]
+            team_id = match["matchInfo"]["team1"]["teamId"]
+            if 'currBatTeamId' in match["matchInfo"]:
+                current_bat_team_id = match["matchInfo"]["currBatTeamId"]
+
+            ipl_match = {
+                "series_name": series_name,
+                "match_format": match_format,
+                "state": state,
+                "status": status,
+                "team1_name": team1_name,
+                "team2_name": team2_name,
+                "team1_sname": team1_sname,
+                "team2_sname": team2_sname,
+                "team_id": team_id,
+            }
+            if 'matchScore' in match:
+                ipl_match["match_score"] = match_score
+                return render_template('updates.html', user=user, teams=teams, ipl_matches=ipl_matches, team1img=team1img, team2img=team2img, match = match, ipl_match= ipl_match, match_score=match_score)       
+
+            print(ipl_match)
+            cur.execute("SELECT imgurl FROM teams WHERE teamabbrv = %s", [team1_sname])
+            team1img = cur.fetchone()['imgurl']
+            cur.execute("SELECT imgurl FROM teams WHERE teamabbrv = %s", [team2_sname])
+            team2img = cur.fetchone()['imgurl']
+            return render_template('updates.html', user=user, teams=teams, ipl_matches=ipl_matches, team1img=team1img, team2img=team2img, match = match, ipl_match= ipl_match)       
+        
+        
+        return render_template('updates.html', user=user, teams=teams)
+    cur.execute("SELECT * FROM teams")
+    teams = cur.fetchall()
+    
+    return render_template('updates.html', teams=teams)
+@app.route('/admin/upload', methods=['GET', 'POST'])
+@basic_auth.required
+def upload():
+    if request.method == 'POST':
+        if 'video' not in request.files:
+            return 'No video file uploaded'
+
+        video_file = request.files['video']
+        if video_file.filename == '':
+            return 'No video file selected'
+
+        video_filename = secure_filename(video_file.filename)
+        video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_filename)
+        video_file.save(video_path)
+
+        vr_filename = 'vr_' + video_filename
+        vr_path = os.path.join(app.config['UPLOAD_FOLDER'], vr_filename)
+        convert_to_vr(video_path, vr_path)
+
+        return 'Video uploaded and converted successfully'
+
+    return render_template('upload.html')
+
+def convert_to_vr(input_file, output_file):
+    conversion_command = f'ffmpeg -i {input_file} -c:v libx264 -b:v 5M -vf "v360=e:e:yaw=180" {output_file}'
+    subprocess.call(conversion_command, shell=True)
+
+@app.route('/vr_videos/<filename>')
+def get_vr_video(filename):
+    vr_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_file(vr_file_path)
+
+@app.route('/vr', methods=['GET'])
+def vr():
+    vr_video_files = get_vr_video_files()
+    if not vr_video_files:
+        return 'No VR videos available'
+
+    latest_vr_video = vr_video_files[-1]
+    return render_template('vr.html', latest_vr_video=latest_vr_video)
+
+def get_vr_video_files():
+    vr_files = []
+    for file in os.listdir(app.config['UPLOAD_FOLDER']):
+        if file.startswith('vr_') and file.endswith('.mp4'):
+            vr_files.append(file)
+    return vr_files
+
+
+@app.route('/post/event/unfollow', methods=['GET', 'POST'])
+def unfollow():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT following FROM users WHERE email = %s", [session['email']])
+    follteam = cur.fetchone()
+    if follteam['following'] != None:
+        cur.execute("SELECT * FROM teams WHERE teamabbrv = %s", [follteam['following']])
+        targetteam=cur.fetchone()
+        cur.execute("UPDATE teams SET followers = %s WHERE teamabbrv = %s", [targetteam['followers']-1 ,targetteam['teamabbrv']])
+        mysql.connection.commit()
+        cur.execute("UPDATE users SET following = %s WHERE email = %s", [None ,session['email']])
+        mysql.connection.commit()
+        return 'ee', 200
+    return 'ee', 500
+
+@app.route('/book/tickets', methods=['GET', 'POST'])
+def book():
+    if 'loggedin' in session:
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM users WHERE email=%s", (session['email'],))
+        user = cur.fetchone()
+        cur.execute("SELECT * FROM teams WHERE id=%s", (user['following'],))
+        teams = cur.fetchone()
+        url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/upcoming"
+        upresponse = requests.get(url, headers=headers)
+        upresponse = (upresponse.json())
+        ipl_matches = []
+        for match_type in upresponse['typeMatches']:
+            if match_type['matchType'] == 'League':
+                for series_match in match_type['seriesMatches']:
+                    if series_match['seriesAdWrapper']['seriesName'] == 'Indian Premier League 2023':
+                        for match in series_match['seriesAdWrapper']['matches']:
+                            ipl_matches.append(match)
+                                
+        for match in ipl_matches:
+            series_name = match["matchInfo"]["seriesName"]
+            match_id = match["matchInfo"]["matchId"]
+            match_format = match["matchInfo"]["matchFormat"]
+            state = match["matchInfo"]["state"]
+            team1_name = match["matchInfo"]["team1"]["teamName"]
+            team2_name = match["matchInfo"]["team2"]["teamName"]
+            team1_sname = match["matchInfo"]["team1"]["teamSName"]
+            team2_sname = match["matchInfo"]["team2"]["teamSName"]
+            if 'matchScore' in match:
+                match_score = match['matchScore']["team1Score"]["inngs1"]
+            team_id = match["matchInfo"]["team1"]["teamId"]
+            if 'currBatTeamId' in match["matchInfo"]:
+                current_bat_team_id = match["matchInfo"]["currBatTeamId"]
+
+            ipl_match = {
+                "series_name": series_name,
+                "match_format": match_format,
+                "state": state,
+                "team1_name": team1_name,
+                "team2_name": team2_name,
+                "team1_sname": team1_sname,
+                "team2_sname": team2_sname,
+                "team_id": team_id,
+            }
+            print(ipl_matches)
+            cur.execute("SELECT imgurl FROM teams WHERE teamabbrv = %s", [team1_sname])
+            team1img = cur.fetchone()['imgurl']
+            cur.execute("SELECT imgurl FROM teams WHERE teamabbrv = %s", [team2_sname])
+            team2img = cur.fetchone()['imgurl']
+            if 'matchScore' in match:
+                ipl_match["match_score"] = match_score
+                return render_template('book.html', user=user, teams=teams, ipl_matches=ipl_matches, team1img=team1img, team2img=team2img, match = match, ipl_match= ipl_match, match_score=match_score)       
+            return render_template('book.html', user=user, teams=teams, ipl_matches=ipl_matches, team1img=team1img, team2img=team2img, match = match, ipl_match= ipl_match)       
+        return render_template('book.html', user=user, team=team)
+        # return render_template ("book.html")
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    return render_template('book.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 app.run(debug=True)
